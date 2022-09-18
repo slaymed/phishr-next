f985d5cd9e342ef439ffcd760aa90819b9dc263d
Mohamed Bedr <mohabedr34@gmail.com>
2022-09-18 21:41:18 +0100

    Applied Footer Update

--- components/Component4.js
+++ components/Component4.js
@@ -3,10 +3,7 @@ import React from "react";
 const Component4 = () => {
     return (
         <div className="component4" align="center">
-            <p className="developerInfo" style={{ paddingBottom: 20 }}>
-                {" "}
-                Developed by Swetha Shiva Prakash{" "}
-            </p>
+            <p className="developerInfo">Project by: Swetha Shiva Prakash</p>
         </div>
     );
 };
